3df29b11bdae0f02b8084ce7fea5839ff208b321
shendyeff <shendyteuku2@gmail.com>
2023-11-20 01:27:12 +0700

    menambahkan fitur form spreadsheet

--- js/script.js
+++ js/script.js
@@ -52,3 +52,18 @@ window.onscroll = () => {
     this.innerHeight + this.scrollY >= document.scrollingElement.scrollHeight
   );
 };
+
+window.addEventListener("load", function () {
+  const form = document.getElementById("my-form");
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    const data = new FormData(form);
+    const action = e.target.action;
+    fetch(action, {
+      method: "POST",
+      body: data,
+    }).then(() => {
+      alert("Successfully submitted the form!");
+    });
+  });
+});
