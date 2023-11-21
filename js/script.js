378f0053fec33361cc9a9fb89ee7400db1773338
shendyeff <shendyteuku2@gmail.com>
2023-11-21 20:27:59 +0700

    memperbarui connect ke email

--- js/script.js
+++ js/script.js
@@ -53,17 +53,53 @@ window.onscroll = () => {
   );
 };
 
-window.addEventListener("load", function () {
-  const form = document.getElementById("my-form");
-  form.addEventListener("submit", function (e) {
-    e.preventDefault();
-    const data = new FormData(form);
-    const action = e.target.action;
-    fetch(action, {
-      method: "POST",
-      body: data,
-    }).then(() => {
-      alert("Successfully submitted the form!");
-    });
+//connect spreadsheet
+// window.addEventListener("load", function () {
+//   const form = document.getElementById("my-form");
+//   form.addEventListener("submit", function (e) {
+//     e.preventDefault();
+//     const data = new FormData(form);
+//     const action = e.target.action;
+//     fetch(action, {
+//       method: "POST",
+//       body: data,
+//     }).then(() => {
+//       alert("Successfully submitted the form!");
+//     });
+//   });
+// });
+
+//connect ke email
+const form = document.querySelector("form");
+const fullName = document.getElementById("name");
+const emailAddress = document.getElementById("email");
+const phoneNumber = document.getElementById("phone");
+const subjectEmail = document.getElementById("subject");
+const yourMessage = document.getElementById("message");
+
+function sendEmail() {
+  const bodyMessage = `Full Name: ${fullName.value}<br> Email Address: ${emailAddress.value}<br>
+  Phone Number: ${phoneNumber.value}<br> Message: ${yourMessage.value}<br>`;
+
+  Email.send({
+    SecureToken: "485fd453-3bde-45ac-91df-e873bd439b01",
+    To: "shendyteuku2@gmail.com",
+    From: "shendyteuku2@gmail.com",
+    Subject: subjectEmail.value,
+    Body: bodyMessage,
+  }).then((message) => {
+    if (message == "OK") {
+      Swal.fire({
+        title: "Success!",
+        text: "Message sent successfully!",
+        icon: "success",
+      });
+    }
   });
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  sendEmail();
 });
